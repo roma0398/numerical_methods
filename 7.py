098f8a24664ddf80b47da9837cc5353eb801b058
Роман <roma0398@mail.ru>
2019-03-22 20:58:09 +0300

    First commit for 7 task

--- 7.py
+++ 7.py
@@ -1,6 +1,6 @@
 from math import *
 import scipy.integrate as spint
-from sympy import diff
+from sympy import *
 import numpy as np
 
 a = 1.5
@@ -8,7 +8,8 @@ b = 3.3
 alpha = 1 / 3
 beta = 0
 
-
+x = symbols('x')
+f = 2 * cos(2.5 * x) * exp(x * alpha) + 4 * sin(3.5 * x) * exp(-3 * x) + x
 def f(x):
     return 2 * cos(2.5 * x) * exp(x * alpha) + 4 * sin(3.5 * x) * exp(-3 * x) + x
 
@@ -44,35 +45,35 @@ def p_omega_dfx(x):
 def abs_p_omega(x):
     return abs(p(x) * omega(x))
 
-
+N = 3
 x1 = a
 x2 = (a + b) / 2
 x3 = b
+X = np.linspace(a, b, N)
 I = spint.quad(F, a, b)[0]
 print("Точное значение интеграла", I)
 
 mu1 = spint.quad(p, a, b)[0]
 mu2 = spint.quad(px, a, b)[0]
 mu3 = spint.quad(pxx, a, b)[0]
-M1 = np.array([[1, 1, 1], [x1, x2, x3], [x1 ** 2, x2 ** 2, x3 ** 2]])
+M1 = np.array([[1, 1, 1], X, [x1 ** 2, x2 ** 2, x3 ** 2]])
 v1 = np.array([mu1, mu2, mu3])
 A = np.linalg.solve(M1, v1)
-X = [x1, x2, x3]
 sum = 0
-for i in range(3):
+for i in range(N):
     sum += A[i] * f(X[i])
 print("Вычисленное начение интеграла", sum)
 expr = diff(
     diff(diff("2 * cos(2.5 * x) * exp(x / 3) + 4 * sin(3.5 * x) * exp(-3 * x) + x"))
 )
-dfx_e = dfx(x2)
-R_1 = spint.quad(p_omega_dfx, a, b)[0] / factorial(3)
+dfx_e = dfx(X[N//2])
+R_1 = spint.quad(p_omega_dfx, a, b)[0] / factorial(N)
 print("Значение точной методической погрешности", R_1)
-x_area = np.linspace(a, b, 2000)
+x_area = np.linspace(a, b, N)
 dd = []
 for i in x_area:
     dd.append(abs(dfx(i)))
 M = max(dd)
-R = (M / factorial(3)) * spint.quad(abs_p_omega, a, b)[0]
+R = (M / factorial(N)) * spint.quad(abs_p_omega, a, b)[0]
 print("Значение оценочной методической погрешности ", R)
 print("Разность точного интеграла и вычисленного", I - sum)
